5023e8a9a68eb4819b1560be9010fae7d0a4d9c7
PIYUSH KHURANA <52041719+PIYUSH-01@users.noreply.github.com>
2019-08-25 13:02:38 +0530

    Add files via upload

---
+++ COLOR manager/ColorPickerVC.swift
@@ -0,0 +1,22 @@
+//
+//  ColorPickerVC.swift
+//  protocolizer
+//
+
+import UIKit
+
+class ColorPickerVC: UIViewController {
+    
+    var delegate: ColorTranferDelegate? = nil
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func buttonWasPressed(sender: UIButton) {
+        if delegate != nil {
+            delegate?.userDidChoose(color: sender.backgroundColor!, withName: sender.titleLabel!.text!)
+            self.navigationController?.popViewController(animated: true)
+        }
+    }
+}
---
+++ COLOR manager/ColorTransferDelegate.swift
@@ -0,0 +1,11 @@
+//
+//  ColorTransferDelegate.swift
+//  protocolizer
+//
+//
+
+import UIKit
+
+protocol ColorTranferDelegate {
+    func userDidChoose(color: UIColor, withName colorName: String)
+}
